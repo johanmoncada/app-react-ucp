af5effb6f6805f656bf0c4126b51eb743ccffbe1
Johan Moncada <johan.moncadat@gmail.com>
2025-05-17 16:10:59 -0500

    Add integrantes paragraph to App component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
       <div>
         <h1>¡Universidad Católica de Pereira !</h1>
         <p>Listado de integrantes - Proceso de Desarrollo de Software I</p>
+        <p>Integrantes:</p>
       </div>
     </>
   );
